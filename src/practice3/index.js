d22f5bfb750e15ad9634f3aa3a50133e73fe680b
fengpei0114 <feng_pei0114@163.com>
2020-11-11 22:44:38 +0800

    finish practice4

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,3 +1,12 @@
 export const getPoetry = () => {
-
-}
+  return fetch("https://v1.jinrishici.com/all.json", {
+    method: "GET",
+    headers: {
+      accept: "text/html",
+    },
+    credentials: "same-origin",
+  })
+    .then((data) => data.json())
+    .then((data) => [data.origin, data.author, data.content])
+    .catch((error) => alert(error.message));
+};
